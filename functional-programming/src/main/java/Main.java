09f38177e6fc8281854ed0b786680cc1611867ba
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2022-04-13 16:28:52 -0400

    Trying to push to stage

--- functional-programming/src/main/java/Main.java
+++ functional-programming/src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("I am on the adam branch...");
+        System.out.println("Pushing to Stage");
 
         System.out.println("Employee: " + employee);
         System.out.println("Employee1: " + employee1);
